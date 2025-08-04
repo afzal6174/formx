14a59f575205c835e5cbc5b9e422bd19b2145843
afzal <m.afzal6174@gmail.com>
2025-08-04 15:34:05 +0600

    update: update logo component

--- src/components/header/logo.jsx
+++ src/components/header/logo.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
-  return <div className="font-bold text-3xl">FormX</div>;
+  return (
+    <div className="font-bold text-3xl">
+      <Link href="/">
+        Form
+        <span className="text-accent-foreground">X</span>
+      </Link>
+    </div>
+  );
 };
 
 export default Logo;
